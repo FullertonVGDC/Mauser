da0858da9308e30e7c9f485c009b6db4f2a5ffeb
Michael Perez <mperez980089@yahoo.com>
2018-10-30 02:32:38 -0700

    Finished cleaning up The Wall, added bee entities

---
+++ Assets/scripts/Bee.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+    [HideInInspector]
+    public float deg;
+    float swaySpeed = 300;
+    [HideInInspector]
+    public Vector2 anchorPos;
+
+    void Start()
+    {
+        anchorPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        deg += swaySpeed * Time.deltaTime;
+        if (deg >= 360) deg -= 360;
+
+        float finalSin = 0.5f * Mathf.Sin(deg * Mathf.Deg2Rad);
+        transform.localPosition = new Vector3(anchorPos.x, anchorPos.y + finalSin, transform.localPosition.z);
+    }
+}
