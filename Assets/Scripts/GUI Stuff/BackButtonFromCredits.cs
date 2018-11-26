75d636f9c772dc7bfd50fe74cb1a5cab24fc5dfd
solid256 <connorgresko@gmail.com>
2018-11-25 20:10:30 -0800

    Added a main menu with bare basics.

---
+++ Assets/Scripts/GUI Stuff/BackButtonFromCredits.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BackButtonFromCredits : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Button button4 = GetComponent<Button> ();
+
+		button4.onClick.AddListener(clickEventListener);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	private void clickEventListener()
+	{
+		SceneManager.LoadScene ("main_menu");
+	}
+}
---
+++ Assets/Scripts/GUI Stuff/QuitButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Button button4 = GetComponent<Button> ();
+
+		button4.onClick.AddListener(clickEventListener);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	private void clickEventListener()
+	{
+		Application.Quit();
+	}
+}
