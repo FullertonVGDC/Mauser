df978daf3d570ef59a9606d08f3c7cbf95e1c6bd
Michael Perez <mperez980089@yahoo.com>
2018-11-30 17:03:06 -0800

    Reorganized gracie's stuff

---
+++ Assets/Scripts/Gracie's Scripts/spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "projectile") {
+			 // gameObject.GetComponent<shooting>().score++;
+			Destroy(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Rigidbody2D>();
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
