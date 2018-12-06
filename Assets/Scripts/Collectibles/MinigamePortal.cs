5c37444865c586dc5d781dfc0befbcf5ff63f60f
Michael Perez <mperez980089@yahoo.com>
2018-12-05 16:16:32 -0800

    Updated minigame portal graphics

--- Assets/Scripts/Collectibles/MinigamePortal.cs
+++ Assets/Scripts/Collectibles/MinigamePortal.cs
@@ -5,27 +5,49 @@ using UnityEngine;
 public class MinigamePortal : MonoBehaviour
 {
     Player player;
+    bool canTouch;
     bool activated;
+    public float rotationSpeed;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+        transform.localScale = new Vector3(0, 0, 0);
+        LeanTween.scale(gameObject, new Vector3(2, 2, 2), 2).setEase(LeanTweenType.easeOutCubic).setOnComplete(() =>
+        {
+            canTouch = true;
+        });
+
+        LeanTween.value(rotationSpeed * 20, rotationSpeed, 2).setEase(LeanTweenType.easeOutCubic).setOnUpdate((float val) =>
+        {
+            rotationSpeed = val;
+        });
+
+        LeanTween.delayedCall(6, () =>
+        {
+            LeanTween.scale(gameObject, new Vector3(0, 0, 0), 8).setEase(LeanTweenType.easeInOutCubic).setOnComplete(() =>
+            {
+                Destroy(gameObject);
+            });
+        });
     }
 
     void Update()
     {
-
+        transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            if (!activated)
+            if (!activated && canTouch)
             {
                 if (!player.GetIsDead())
                 {
                     activated = true;
+                    LeanTween.cancelAll();
 
                     GameObject.Find("GuiFader(Clone)").GetComponent<GuiFader>().SetIsFadingIn(true);
                     player.GetComponent<AudioSource>().PlayOneShot(player.mMauserFinishLevelAudioClip, 1.0f);
