2ae9d832f728ef9757c05a6f97f0796e52048396
Michael Perez <mperez980089@yahoo.com>
2018-12-07 06:03:27 -0800

    Refactored pause menu slightly, made debug cap possible to collect

---
+++ Assets/Scripts/GUI Stuff/PauseMenuHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuHandler : MonoBehaviour
+{
+    public void ResumeButton()
+    {
+        GameObject.Find("GameplayGUI").GetComponent<GameplayGUI>().SetPaused(false);
+        GlobalData.instance.GetComponent<AudioSource>().volume = 0.8f;
+    }
+
+    public void QuitButton()
+    {
+        GlobalData.instance.StopMusic();
+        SceneManager.LoadScene("main_menu");
+    }
+}
