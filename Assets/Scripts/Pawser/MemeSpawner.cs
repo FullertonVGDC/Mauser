f5d8281d68274437a616802c584b208047ff6107
Michael Perez <mperez980089@yahoo.com>
2019-05-24 21:46:41 -0700

    Added meme spawner

---
+++ Assets/Scripts/Pawser/MemeSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemeSpawner : MonoBehaviour
+{
+    public Sprite[] memes;
+    public SpriteRenderer leftMeme;
+    public SpriteRenderer rightMeme;
+
+    void Start()
+    {
+        leftMeme.sprite = memes[Random.Range(0, memes.Length - 1)];
+        rightMeme.sprite = memes[Random.Range(0, memes.Length - 1)];
+    }
+}
